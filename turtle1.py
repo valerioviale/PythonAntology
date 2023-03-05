fdb7003b111e444e878ada3fc6496c2dfc63f2c5
valerioviale <34212301+valerioviale@users.noreply.github.com>
2023-03-05 14:42:35 +0100

    Add files via upload
    Draw a square using Turtle

---
+++ turtle1.py
@@ -0,0 +1,17 @@
+from turtle import *
+import time
+
+forward(200)
+time.sleep(2)
+left(90)
+time.sleep(2)
+forward(200)
+time.sleep(2)
+left(90)
+time.sleep(2)
+forward(200)
+time.sleep(2)
+left(90)
+time.sleep(2)
+forward(200)
+time.sleep(2)
