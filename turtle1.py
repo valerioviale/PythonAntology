d9969bf142efa916833b93cd0c1621be24dcc536
valerioviale <34212301+valerioviale@users.noreply.github.com>
2023-03-05 17:06:17 +0100

    Update turtle1.py

--- turtle1.py
+++ turtle1.py
@@ -1,17 +1,18 @@
 from turtle import *
 import time
 
-forward(200)
-time.sleep(2)
-left(90)
-time.sleep(2)
-forward(200)
-time.sleep(2)
-left(90)
-time.sleep(2)
-forward(200)
-time.sleep(2)
-left(90)
-time.sleep(2)
-forward(200)
-time.sleep(2)
+from turtle import * # Loads commands used to draw
+
+color('red') # Sets drawing color to red
+forward(200) # Moves the turtle 200 pixels forward, i.e. right
+left(90) # Turn left 90 degrees
+# Turtle is now facing up
+forward(200) # Moves 200 pixels forward, i.e. up 
+left(90) # Turn left 90 degrees
+# Turtle now facing left 
+forward(200) # Moves 200 pixels forward, i.e. left
+left(90) # Turn left 90 degrees
+# Turtle now facing down
+forward(200) # Complete bottom of the square
+time.sleep(5)
+bye() # Tells the turtle we're done writing commands
