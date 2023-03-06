0a3ef568b1092205f0f84e7438fda01cf603fcac
valerioviale <34212301+valerioviale@users.noreply.github.com>
2023-03-06 10:38:49 +0100

    Create turtle5.py
    A code to write the letter D

---
+++ turtle5.py
@@ -0,0 +1,20 @@
+""" A code to write the letter D
+ """
+from turtle import *
+
+left(90)   
+forward(200)
+right(90)
+forward(100)
+
+right(45)
+forward(71)
+right(45)
+forward(100) 
+right(45)
+forward(71)
+right(45)
+forward(100)
+hideturtle()
+
+bye()
