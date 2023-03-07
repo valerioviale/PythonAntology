418dea709ca432a1f487fb3edd67da93f50ef211
valerioviale <34212301+valerioviale@users.noreply.github.com>
2023-03-07 12:34:15 +0100

    Update turtle6.py
    Improved the precision of the circle

--- turtle6.py
+++ turtle6.py
@@ -1,17 +1,16 @@
 from turtle import *
+import time
 width(5)
 
 left(180)
-
+color("blue")
 # Start of for loop.
-for i in range(5): # Code below is repeated 5 times
+for i in range(128): # Code below is repeated 5 times
     # Code to be repeated 
-    forward(50) # Indentation indcates code is inside the loop
-    left(45) # Still inside the for loop
-for j in range(5): # Code below is repeated 5 times
-    # Code to be repeated 
-    forward(50) # Indentation indcates code is inside the loop
-    left(45) # Still inside the for loop
+    forward(7) # Indentation indcates code is inside the loop
+    left(3.14) # Still inside the for loop
+
 # No more indentation! Marks the end of the for-loop.
+time.sleep(5)
 hideturtle()
 bye()
