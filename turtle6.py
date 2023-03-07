8388eeee6df60bb1e44015f1d443f618e872a209
valerioviale <34212301+valerioviale@users.noreply.github.com>
2023-03-07 12:48:26 +0100

    Update turtle6.py
    4 colored circles

--- turtle6.py
+++ turtle6.py
@@ -5,10 +5,34 @@ width(5)
 left(180)
 color("blue")
 # Start of for loop.
-for i in range(128): # Code below is repeated 5 times
+for i in range(40): # Code below is repeated 40 times
     # Code to be repeated 
-    forward(7) # Indentation indcates code is inside the loop
-    left(3.1415926535897932384626433) # Still inside the for loop
+    forward(30) # Indentation indcates code is inside the loop
+    left(11.25) # Still inside the for loop
+
+right(180)
+color("red")
+# Start of for loop.
+for i in range(40): # Code below is repeated 40 times
+    # Code to be repeated 
+    forward(30) # Indentation indcates code is inside the loop
+    left(11.25) # Still inside the for loop
+
+left(180)
+color("green")
+# Start of for loop.
+for i in range(40): # Code below is repeated 40 times
+    # Code to be repeated 
+    forward(30) # Indentation indcates code is inside the loop
+    left(11.25) # Still inside the for loop
+
+left(180)
+color("fuchsia")
+# Start of for loop.
+for i in range(40): # Code below is repeated 40 times
+    # Code to be repeated 
+    forward(30) # Indentation indcates code is inside the loop
+    left(11.25) # Still inside the for loop
 
 # No more indentation! Marks the end of the for-loop.
 time.sleep(5)
